a6ce3b841a7b142f0513e9538ed8b8836c983d83
Jxxunnn <86228307+Jxxunnn@users.noreply.github.com>
2022-02-17 17:38:24 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,49 +1,42 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "./App.css";
 import Carousel from "./Carousel/Carousel";
 
-const hardCalculate = (number) => {
-  console.log("어려운 계산");
-  for (let i = 0; i < 999999999; i++) {}
-  return number + 10000;
-};
-
-const easyCalculate = (number) => {
-  console.log("쉬운 계산");
-  return number + 1;
-};
-
 function App() {
-  const [hardNumber, setHardNumber] = useState(1);
-  /*  const hardSum = hardCalculate(hardNumber); */
-  const hardSum = useMemo(() => {
-    return hardCalculate(hardNumber);
-  }, [hardNumber]);
-  const [easyNumber, setEasyNumber] = useState(1);
-  const easySum = easyCalculate(easyNumber);
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
+  const location = useMemo(() => {
+    return {
+      country: isKorea ? "한국" : "외국",
+    };
+  }, [isKorea]);
+  const location = {
+    contry: isKorea ? "한국" : "외국",
+  };
 
+  useEffect(() => {
+    console.log("useEffect 호출");
+  }, [location]);
   return (
     <div className="App">
-      <div>
-        <h3>어려운 계산기</h3>
-        <input
-          type="number"
-          value={hardNumber}
-          onChange={(e) => {
-            setHardNumber(e.target.value);
-          }}
-        ></input>
-        <span> + 10000 = {hardSum}</span>
-        <h3>쉬운 계산기</h3>
-        <input
-          type="number"
-          value={easyNumber}
-          onChange={(e) => {
-            setEasyNumber(e.target.value);
-          }}
-        ></input>
-        <span> + 10000 = {easySum}</span>
-      </div>
+      <h2>하루에 몇 끼 먹어요?</h2>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => {
+          setNumber(e.target.value);
+        }}
+      ></input>
+      <hr />
+      <h2>어느 나라에 있어요?</h2>
+      <p>나라: {location.contry}</p>
+      <button
+        onClick={() => {
+          setIsKorea(!isKorea);
+        }}
+      >
+        비행기 타자
+      </button>
     </div>
   );
 }
