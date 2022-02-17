cbc7b61c403b98ec950d894bbd3414ad7c934798
Jxxunnn <86228307+Jxxunnn@users.noreply.github.com>
2022-02-17 16:02:39 +0900

    Carousel
    1.useRef로 DOM요소 조작하여 container의 offsetWidth값 가져옴.
    2.useEffect로 렌더링 될 때의 offsetWidth값 저장.
    3.각 슬라이드의 상태값을 useState에 담아 translateX할 때 데이터 바인딩 해줌~ 끝!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
+import Carousel from "./Carousel/Carousel";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <Carousel></Carousel>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -8,41 +8,56 @@ import img4 from "./4.jpeg";
 //useRef에 대해 더 공부하고 내일 carousel 끝내기!
 
 function Carousel() {
-  let [배너, 배너변경] = useState([img1, img2, img3, img4]);
-  let [현재슬라이드, 현재슬라이드변경] = useState(0);
-  let [x, x변경] = useState(0);
-  let 초기값 = -100;
-  let countRef = useRef(null);
-  let totalSlides = 배너.length;
+  const [배너, 배너변경] = useState([img1, img2, img3, img4]);
+  const [현재슬라이드, 현재슬라이드변경] = useState(0);
+  const [width, setWidth] = useState(0);
+  const [x, x변경] = useState(0);
+  const bannerWidthRef = useRef();
+  const countRef = useRef(1);
 
-  function prevSlide() {
-    x변경(100);
-    초기값 += 100;
-    return 초기값;
-  }
-  function nextSlide() {
-    x변경(초기값);
-    초기값 -= 100;
-    console.log(초기값);
-    return 초기값;
-  }
+  const prevSlide = () => {
+    if (현재슬라이드 > 0) {
+      현재슬라이드변경(현재슬라이드 - 1);
+    } else {
+      현재슬라이드변경(배너.length - 1);
+    }
+    console.log(현재슬라이드);
+  };
+  const nextSlide = () => {
+    if (현재슬라이드 < 3) {
+      현재슬라이드변경(현재슬라이드 + 1);
+    } else {
+      현재슬라이드변경(0);
+    }
+  };
+
+  useEffect(() => {
+    setWidth(-bannerWidthRef.current.offsetWidth);
+  }, [현재슬라이드]);
 
   return (
     <div className={styles.container}>
       <div
-        style={{ transform: `translateX(${x}%)` }}
+        ref={bannerWidthRef}
+        style={{
+          transform: `translateX(${width * 현재슬라이드}px)`,
+        }}
         className={styles.slideContainer}
       >
         <div>
+          <h1>11111111</h1>
           <img className={styles.banner} src={배너[0]}></img>
         </div>
         <div>
+          <h1>22222222</h1>
           <img className={styles.banner} src={배너[1]}></img>
         </div>
         <div>
+          <h1>33333333333</h1>
           <img className={styles.banner} src={배너[2]}></img>
         </div>
         <div>
+          <h1>44444444</h1>
           <img className={styles.banner} src={배너[3]}></img>
         </div>
       </div>
